8b4fa07eea7f31dcb90e6a27c7b0cfc2a07321e5
kuwabara-hirokazu <h.kuwabara1103@gmail.com>
2022-01-09 15:12:11 +0900

    ポケモンデータ数を追加

--- app/src/main/java/com/example/sampleweather/data/MockPokemonData.kt
+++ app/src/main/java/com/example/sampleweather/data/MockPokemonData.kt
@@ -4,6 +4,6 @@ const val POKEMON_IMAGE =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
 
 fun createPokemonData(): String {
-    val pokemonGuideId = (1..151).random()
+    val pokemonGuideId = (1..386).random()
     return "${POKEMON_IMAGE + pokemonGuideId}.png"
 }
